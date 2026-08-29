e6d72d95a36d6cb92a53953a32800d8893f9a2e4
bolfab13 <bolfabgfx@gmail.com>
2026-08-29 22:57:53 +0200

    Add config.js with Supabase credentials

---
+++ config.js
@@ -0,0 +1,4 @@
+// ⚠️ À REMPLIR : va dans ton projet Supabase > Project Settings > API
+// Copie "Project URL" et "anon public key" ci-dessous.
+const SUPABASE_URL = "https://gjhnptwgfcmrzdwnwutt.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqaG5wdHdnZmNtcnpkd253dXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NDQ5OTEsImV4cCI6MjEwMzMyMDk5MX0.2tNR50RCsEFj4sJhrLLcLjkLqT-7RgmsIH39FjmWGHw";
